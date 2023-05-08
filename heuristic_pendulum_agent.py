583e33c93d342d4687e2d8cbd0c037f268f625b7
Your Name <milad.shafiee@epfl.ch>
2023-05-08 10:57:45 +0200

    Heuristic Policy

--- heuristic_pendulum_agent.py
+++ heuristic_pendulum_agent.py
@@ -7,10 +7,11 @@ class HeuristicPendulumAgent:
         self.action_size = env.action_space.shape[0]
         
     def compute_action(self, state):
-        print(state)
-        if (state[0]<=0):
+        if (state[0]<=0): 
+            # if the pendulum is lower side of the page, apply a constant torque with the same sign of the angular velocity
             action = np.sign(state[2])*0.8
         else:
+            # if the pendulum is upper side of the page, apply a constant torque with the negative sign of the angular velocity
             action = np.sign(state[2])*-0.8
         
         return  action
